42db094bc3e02de205806d82e1ffbd50c888ca22
godwinsodogandji <jeansodogandji@gmail.com>
2024-10-22 11:57:59 +0100

    initial

---
+++ app_musik/app_musik_back/src/main/java/bj/highfiveuniversity/app_musik/AppMusikApplication.java
@@ -0,0 +1,13 @@
+package bj.highfiveuniversity.app_musik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMusikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMusikApplication.class, args);
+	}
+
+}
---
+++ app_musik/app_musik_back/src/test/java/bj/highfiveuniversity/app_musik/AppMusikApplicationTests.java
@@ -0,0 +1,13 @@
+package bj.highfiveuniversity.app_musik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMusikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
