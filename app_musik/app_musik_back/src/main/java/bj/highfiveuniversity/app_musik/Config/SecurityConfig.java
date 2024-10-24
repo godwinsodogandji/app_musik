c8fb71691772e953dab60265f39fdf603a513542
godwinsodogandji <jeansodogandji@gmail.com>
2024-10-24 10:31:51 +0100

    code envoyé

--- app_musik/app_musik_back/src/main/java/bj/highfiveuniversity/app_musik/Config/SecurityConfig.java
+++ app_musik/app_musik_back/src/main/java/bj/highfiveuniversity/app_musik/Config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -23,12 +25,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-            .cors() // Active CORS
-            .and()
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/auth/login",  "/api/auth/register").permitAll()
-                .anyRequest().authenticated()
-            );
+                .cors(withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/api/auth/login", "/api/auth/register").permitAll()
+                                .anyRequest().authenticated()
+                );
         return http.build();
     }
 
